31e1670f635118a91abcc2343d00ea62070b2b64
Rong Chen <rongc@cadence.com>
2018-11-13 22:36:36 -0800

    modified the load_data and updated to latest tensorflow

---
+++ train.py
@@ -0,0 +1,97 @@
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, LearningRateScheduler
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.losses import squared_hinge
+import os
+import argparse
+import tensorflow.keras.backend as K
+import numpy as np
+from six.moves import xrange
+
+from models.model_factory import build_model
+from utils.config_utils import Config
+from utils.load_data_new import load_dataset
+
+# parse arguments
+parser = argparse.ArgumentParser(description='Model training')
+parser.add_argument('-c', '--config_path', type=str,
+                default=None, help='Configuration file')
+parser.add_argument('-o' ,'--override',action='store',nargs='*',default=[])
+
+arguments = parser.parse_args()
+override_dir = {}
+
+for s in arguments.override:
+    s_s = s.split("=")
+    k = s_s[0].strip()
+    v = "=".join(s_s[1:]).strip()
+    override_dir[k]=v
+arguments.override = override_dir
+
+
+cfg = arguments.config_path
+cf = Config(cfg, cmd_args = arguments.override)
+
+
+# if necessary, only use the CPU for debugging
+if cf.cpu:
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+# ## Construct the network
+print('Construct the Network\n')
+
+# In[4]:
+model = build_model(cf)
+
+
+
+print('setting up the network and creating callbacks\n')
+
+early_stop = EarlyStopping(monitor='loss', min_delta=0.001, patience=10, mode='min', verbose=1)
+checkpoint = ModelCheckpoint(cf.out_wght_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max', period=1)
+tensorboard = TensorBoard(log_dir='./logs/' + str(cf.tensorboard_name), histogram_freq=0, write_graph=True, write_images=False)
+
+
+(x_train, y_train), (x_valid, y_valid), (x_test, y_test) = load_dataset(cf.dataset)
+
+
+# learning rate schedule
+def scheduler(epoch):
+    if epoch in cf.decay_at_epoch:
+        index = cf.decay_at_epoch.index(epoch)
+        factor = cf.factor_at_epoch[index]
+        lr = K.get_value(model.optimizer.lr)
+        IT = x_train.shape[0]/cf.batch_size
+        current_lr = lr * (1./(1.+cf.decay*epoch*IT))
+        K.set_value(model.optimizer.lr,current_lr*factor)
+        print('\nEpoch {} updates LR: LR = LR * {} = {}\n'.format(epoch+1,factor, K.get_value(model.optimizer.lr)))
+    return K.get_value(model.optimizer.lr)
+    
+lr_decay = LearningRateScheduler(scheduler)
+
+
+#sgd = SGD(lr=cf.lr, decay=cf.decay, momentum=0.9, nesterov=True)
+adam= Adam(lr=cf.lr, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=cf.decay)
+
+print('compiling the network\n')
+model.compile(loss=squared_hinge, optimizer=adam, metrics=['accuracy'])
+
+if cf.finetune:
+    print('Load previous weights\n')
+    model.load_weights(cf.out_wght_path)
+else:
+    print('No weights preloaded, training from scratch\n')
+
+print('(re)training the network\n')
+model.fit(x_train,y_train,
+            batch_size = cf.batch_size,
+            epochs = cf.epochs,
+            verbose = cf.progress_logging,
+            callbacks = [checkpoint, tensorboard,lr_decay],
+            validation_data = (x_valid,y_valid))
+
+
+print('Done\n')
+
+
+
---
+++ utils/load_data_new.py
@@ -0,0 +1,83 @@
+
+import numpy as np
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.datasets import mnist
+
+def load_dataset(dataset):
+	if (dataset == "CIFAR-10"):
+
+		print('Loading CIFAR-10 dataset...')
+		
+		train_set_size = 45000
+		(x_train, y_train), (x_test, y_test) = cifar10.load_data();
+		x_valid = x_train[train_set_size:,:,:,:]
+		y_valid = y_train[train_set_size:,:]
+		x_train = x_train[0:train_set_size,:,:,:]
+		y_train = y_train[0:train_set_size:,:]
+
+		x_train = np.transpose(np.reshape(np.subtract(np.multiply(2. / 255., x_train), 1.), (-1, 3, 32, 32)),(0,2,3,1))
+		x_valid = np.transpose(np.reshape(np.subtract(np.multiply(2. / 255., x_valid), 1.), (-1, 3, 32, 32)),(0,2,3,1))
+		x_test = np.transpose(np.reshape(np.subtract(np.multiply(2. / 255., x_test), 1.), (-1, 3, 32, 32)),(0,2,3,1))
+		# flatten targets
+		y_train = np.hstack(y_train)
+		y_valid = np.hstack(y_valid)
+		y_test = np.hstack(y_test)
+
+		# Onehot the targets
+		y_train = np.float32(np.eye(10)[y_train])
+		y_valid = np.float32(np.eye(10)[y_valid])
+		y_test = np.float32(np.eye(10)[y_test])
+
+		# for hinge loss
+		y_train = 2 * y_train - 1.
+		y_valid = 2 * y_valid - 1.
+		y_test = 2 * y_test - 1.
+		# enlarge train data set by mirrroring
+		x_train_flip = x_train[:, :, ::-1, :]
+		y_train_flip = y_train
+		x_train = np.concatenate((x_train, x_train_flip), axis=0)
+		y_train = np.concatenate((y_train, y_train_flip), axis=0)
+				
+	elif (dataset == "MNIST"):
+
+		print('Loading MNIST dataset...')
+
+		
+		train_set_size = 50000
+		(x_train, y_train), (x_test, y_test) = mnist.load_data();
+		x_valid = x_train[train_set_size:,:,:,:]
+		y_valid = y_train[train_set_size:,:]
+		x_train = x_train[0:train_set_size,:,:,:]
+		y_train = y_train[0:train_set_size:,:]
+		
+
+		x_train = np.transpose(np.reshape(np.subtract(np.multiply(2. / 255., x_train), 1.), (-1, 1, 28, 28)),(0,2,3,1))
+		x_valid = np.transpose(np.reshape(np.subtract(np.multiply(2. / 255., x_valid), 1.), (-1, 1,  28, 28)),(0,2,3,1))
+		x_test = np.transpose(np.reshape(np.subtract(np.multiply(2. / 255., x_test), 1.), (-1, 1,  28, 28)),(0,2,3,1))
+		# flatten targets
+		y_train = np.hstack(y_train)
+		y_valid = np.hstack(y_valid)
+		y_test = np.hstack(y_test)
+
+		# Onehot the targets
+		y_train = np.float32(np.eye(10)[y_train])
+		y_valid = np.float32(np.eye(10)[y_valid])
+		y_test = np.float32(np.eye(10)[y_test])
+
+		# for hinge loss
+		y_train = 2 * y_train - 1.
+		y_valid = 2 * y_valid - 1.
+		y_test = 2 * y_test - 1.
+		# enlarge train data set by mirrroring
+		x_train_flip = x_train[:, :, ::-1, :]
+		y_train_flip = y_train
+		x_train = np.concatenate((x_train, x_train_flip), axis=0)
+		y_train = np.concatenate((y_train, y_train_flip), axis=0)
+
+
+
+
+	else:
+		print("wrong dataset given")
+
+	return (x_train, y_train), (x_valid, y_valid), (x_test, y_test)
